4c8b43b161e3eb653e9524b45d5a1112b064e3d7
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2021-06-29 15:19:57 +0900

    Add files via upload
    O(N^2) ~

---
+++ AlgorithmSelectionSort/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i, j, min, index = 0, temp;
+		int[] array = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+		
+		for (i = 0; i < 10; i++) {
+			min = 9999;
+			for (j = i ; j < 10; j++) {
+				if (min > array[j]) {
+					min = array[j];
+					index = j;
+				}
+			}
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
+		}
+			
+		for (i = 0; i < 10; i++) {
+			System.out.print(array[i] + " ");
+		}
+		
+	}
+	
+
+}
