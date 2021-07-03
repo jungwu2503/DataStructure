d88e81ad8f001f967ce9bb6f258330784ee43378
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2021-07-03 21:46:32 +0900

    Add files via upload
    Temple Hanoi

--- Hanoi/src/Main.java
+++ Hanoi/src/Main.java
@@ -1,7 +1,8 @@
+// Refactoring
 
 public class Main {
 
-	public static void printStack(char name, Stack x, Stack y, Stack z, Console screen) {
+/*	public static void printStack(char name, Stack x, Stack y, Stack z, Console screen) {
 		if (x.getName() == name)
 			screen.drawDisc(name, x);
 		if (y.getName() == name)
@@ -10,25 +11,15 @@ public class Main {
 			screen.drawDisc(name, z);		
 	}
 	
-	public static void moveHanoi(int n, Stack from, Stack temp, Stack to, Console screen) {
-		if (n == 1) {
-			int x = from.pop();
-			to.push(x);
-			screen.resetScreen();
-			printStack('A', from, temp, to, screen);
-			printStack('B', from, temp, to, screen);
-			printStack('C', from, temp, to, screen);
-			screen.show();
-			return;
-		}
-		moveHanoi(n-1, from, to, temp, screen);
-		moveHanoi(1, from, temp, to, screen);
-		moveHanoi(n-1, temp, from, to, screen);
-	}
+	*/
 	
 	public static void main(String[] args) {
 		
-		Stack a = new Stack('A');
+		Temple templeHanoi = new Temple(5);
+		
+		templeHanoi.run();
+		
+/*		Stack a = new Stack('A');
 		Stack b = new Stack('B');
 		Stack c = new Stack('C');
 		Console screen = new Console(5);
@@ -41,7 +32,8 @@ public class Main {
 		screen.drawDisc('A', a);
 		screen.show();
 		
-		moveHanoi(5, a, b, c, screen);
+		moveHanoi(5, a, b, c, screen); */
+		
 	}
 
 }
